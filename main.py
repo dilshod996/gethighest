fe94382ca367bc3e8ed60ecc9dd4d0b3141604e3
Dilshod Bek <59019957+dilshod996@users.noreply.github.com>
2022-03-16 03:06:11 +0000

    new functionality

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# 🚨 Don't change the code below 👇
+
 student_scores = input("Enter number: ").split()
 for num in range(0, len(student_scores)):
   student_scores[num] = int(student_scores[num])
 print(student_scores)  
-# 🚨 Don't change the code above 👆
 
-#Write your code below this row 👇
 
 highest_score = 0
+average_all = 0
 for x in student_scores:
+  average_all +=1
   if x > highest_score:
     highest_score = x
-print(highest_score)  
-
+    result = highest_score / average_all
+print(highest_score) 
+print(average_all)
+print(result)
 
